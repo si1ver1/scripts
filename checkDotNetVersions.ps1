0efc7de84123d2f63aa81db11d6ddc26cef199f1
si1ver1 <39344314+si1ver1@users.noreply.github.com>
2023-03-13 15:53:53 -0700

    Update checkDotNetVersions.ps1

--- checkDotNetVersions.ps1
+++ checkDotNetVersions.ps1
@@ -1,6 +1,3 @@
-# Allow script to run temporarily
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
-
 $Values = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}'} | Select PSChildName, version
 $Values |Format-Table | Out-String | Write-Host
 
